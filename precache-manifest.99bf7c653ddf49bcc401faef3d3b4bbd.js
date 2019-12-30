f605ef40cd7b11dbb04b6b75719c3336db3ce264
Ferran <febouge@protonmail.ch>
2019-12-30 17:52:50 +0100

    Updates

---
+++ precache-manifest.99bf7c653ddf49bcc401faef3d3b4bbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72202353f0b6b89517b88439e9671386",
+    "url": "/tramuntana/index.html"
+  },
+  {
+    "revision": "6bc5035533cea59cc602",
+    "url": "/tramuntana/static/css/main.696bbd84.chunk.css"
+  },
+  {
+    "revision": "bad0ff8960a5428f81a0",
+    "url": "/tramuntana/static/js/2.bb2add60.chunk.js"
+  },
+  {
+    "revision": "6bc5035533cea59cc602",
+    "url": "/tramuntana/static/js/main.4144cc3a.chunk.js"
+  },
+  {
+    "revision": "85fb112431f99483a439",
+    "url": "/tramuntana/static/js/runtime-main.adc115b0.js"
+  }
+]);
