6ba1f0c25ecb3977b1f0d76fb95ec0ed9e9a4d46
Ferran <febouge@protonmail.ch>
2019-12-30 18:02:40 +0100

    feat: deploy to github pages

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from '../../assets/images/logo.svg'
+import React, { useState, useEffect, FormEvent } from 'react';
 import './App.css';
+import { getCurrentWeather } from '../../api/clients/current-weather';
+import { CurrentWeather } from '../../api/interfaces/current-weather';
+import { getFiveDayForecast } from '../../api/clients/five-day-forecast';
+import { FiveDayForecast } from '../../api/interfaces/five-day-forecast';
 
 const App: React.FC = () => {
+  const [cityName, setCityName] = useState('');
+  const [weather, setWeather] = useState({} as CurrentWeather);
+  const [cityNameForecast, setCityNameForecast] = useState('');
+  const [forecast, setForecast] = useState({} as FiveDayForecast);
+
+  useEffect(() => {
+    if (cityName.length > 3) {
+      getCurrentWeather({ q: cityName })
+        .then(setWeather);
+    }
+  }, [cityName]);
+
+  useEffect(() => {
+    if (cityNameForecast.length > 3) {
+      getFiveDayForecast({ q: cityNameForecast })
+        .then(setForecast);
+    }
+  }, [cityNameForecast]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>Current weather</h3>
+        <input type="text"
+               placeholder="City"
+               name="city-name"
+               value={cityName}
+               onChange={ (e: FormEvent<HTMLInputElement>) => setCityName(e.currentTarget.value) }
+        />
+        <button>Search weather</button>
+        <div><pre>{ JSON.stringify(weather, null, 2) }</pre></div>
+      </div>
+      <div>
+        <h3>5 day/3 h forecast</h3>
+        <input type="text"
+               placeholder="City"
+               name="city-name"
+               value={cityNameForecast}
+               onChange={ (e: FormEvent<HTMLInputElement>) => setCityNameForecast(e.currentTarget.value)}
+        />
+        <button>Search weather</button>
+        <div><pre>{ JSON.stringify(forecast, null, 2) }</pre></div>
+      </div>
     </div>
   );
 }
